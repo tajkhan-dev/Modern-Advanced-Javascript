63aea67a0b0e69be5d4d86b0999041e15f6fe487
tajkhan-dev <tajkhan.dev@gmail.com>
2023-10-15 22:44:18 +0500

    2nd step datatype and variable created

---
+++ datatypes_variables/variables.js
@@ -0,0 +1,9 @@
+// What is a variable in javascript?
+// In JavaScript, a variable is a container that you can use to store and manipulate data. 
+
+// their are three ways to create a variable i.e
+// var
+// let
+// const
+
+var myname="jamstack developer";
