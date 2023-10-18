501e8f4bee0ffa502bf72b40954f5ae86ad1ed44
tajkhan-dev <tajkhan.dev@gmail.com>
2023-10-18 18:04:59 +0500

    working on operators

---
+++ 03_operators/operators.js
@@ -0,0 +1,32 @@
+// *** Operators ***
+
+
+// Assigment Operators i.e =
+let x = 10; // = is used for Assignment and it assigns the 10 to variable x
+
+// Basic Math Operators i.e +, -, *, /
+
+let number1=5;
+let number2=7;
+console.log(number1+number2);// returns 12 Addition Operator plus is used 
+console.log(number1-number2);// returns -2 Subtraction Operator minus is used
+console.log(number1*number2);// returns 35 Multiplication Operator times is used
+console.log(number1/number2);// returns 0.8 Division Operator slash (/) is used
+
+console.log(number1%number2);// returns 1 (the reminder of the answer after dividing it) Modulus Operator percentage "%" is used 
+
+// Exponentiation Operator **
+
+let base = 2;
+let exponent = 3;
+console.log(base ** exponent); // returns 8 (2 to the power of 3)
+
+// Increment Decrement operators ++ --
+let number3=10;
+++number3; // increment by one
+console.log(number3) // returns 11
+--number3; // decrement by one
+console.log(number3) // returns 10 since number3 was now 11 at line number 22
+
+// pre-increment /decrement and post-increment/decrement
+
